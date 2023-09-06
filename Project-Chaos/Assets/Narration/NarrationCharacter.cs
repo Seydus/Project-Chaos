042a32dba9dae135b84f3741bef56f116b99432b
Seydus <weljohncatanpatan@gmail.com>
2023-09-06 09:07:10 +0800

    _dialogue

---
+++ Project-Chaos/Assets/Narration/NarrationCharacter.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Narration/Character")]
+public class NarrationCharacter : ScriptableObject
+{
+    [SerializeField] private string m_CharacterName;
+
+    public string CharacterName => m_CharacterName;
+}
